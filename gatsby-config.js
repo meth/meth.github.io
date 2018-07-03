f03ce9031abcc1f64916e36f6929457348bddba3
Ramesh Nair <ram@hiddentao.com>
2018-07-03 11:28:26 +0100

    work towards homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'Meth',
+    description: 'Cross-platform Etherum wallet and Dapp browser',
+    keywords: 'ethereum, dapp, blockchain, wallet, browser, mobile, desktop, ios, android'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,13 +4,17 @@ import Link from 'gatsby-link'
 import styles from './index.module.styl'
 
 const Header = ({ siteTitle }) => (
-  <div className={styles.outer}>
-    <div className={styles.inner}>
-      <h1 className={styles.title}>
-        <Link to="/" className={styles.link}>
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className={styles.container}>
+    <Link to="/" className={styles.title}>
+      {siteTitle}
+    </Link>
+    <div className={styles.menu}>
+      <div className={styles.desktop}>
+        <Link to="/" className={styles.link}>Desktop Home</Link>
+      </div>
+      <div className={styles.mobile}>
+        <Link to="/" className={styles.link}>Mobile Home</Link>
+      </div>
     </div>
   </div>
 )

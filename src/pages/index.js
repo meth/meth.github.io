87f4f8006996aa4a229849b5daae909f80833689
Ramesh Nair <ram@hiddentao.com>
2018-07-03 22:32:56 +0100

    add features

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,53 +10,163 @@ export default ({ data }) => (
     <section className={styles.pane1}>
       <section className={styles.pane1_textBox}>
         <p>
-          <em>Ethereum wallet</em> and <em>Dapp browser</em> for your <em className={styles.em2}>desktop</em> and <em className={styles.em2}>mobile</em>.
+          <em>Ethereum wallet</em> and <em>Dapp browser</em> for your <em className={styles.em2}>desktop</em> and <em className={styles.em2}>mobile</em>
         </p>
         <a className={styles.button} href="/download">Download now</a>
       </section>
       <section className={styles.pane1_imageBox}>
-        <Img sizes={data.placeIt.childImageSharp.sizes} />
+        <Img sizes={data.xpApp.childImageSharp.sizes} />
       </section>
     </section>
-    <section className={styles.featurePane}>
+    <section className={styles.featurePaneRight}>
       <section className={styles.info}>
-        <h2 className={styles.infoTitle}>Mnemonic-based HD Wallet</h2>
+        <h2 className={styles.infoTitle}>HD wallet and tokens</h2>
         <ul style={styles.infoList}>
-          <li>Uses industry-standard BIP-44 protocol to generate Ethereum accounts from a secret <em>passphrase</em>.</li>
-          <li>Never lost access - you can also access your accounts at any time from Metamask and MyEtherWallet at any time using the same passphrase.</li>
+          <li>Generate account addresses using industry standard <a href="https://github.com/ethereum/EIPs/issues/84">BIP-44</a> protocol.</li>
+          <li>Supports <a href="https://github.com/meth/app/blob/master/src/config/tokens.json">comprehensive list</a> of ERC-20 tokens as your own custom tokens.</li>
+          <li>Never lose access - use your passphrase with Metamask and MyEtherWallet.</li>
         </ul>
       </section>
       <section className={styles.screenshot}>
-        <Img sizes={data.generateWalletScr.childImageSharp.sizes} />
+        <Img sizes={data.xpWallet.childImageSharp.sizes} />
       </section>
     </section>
-    <ul>
-      <li>, portable to Metamask and MyEtherWallet</li>
-      <li>Secure backup and sync with client-side encryption</li>
-      <li>ERC-20 token support, with ability to add and save custom tokens</li>
-      <li>Deploy new contracts and interact with existing ones </li>
-      <li>Multi-tab Dapp browser with bookmarks</li>
-      <li>4-digit personal security code for additional security</li>
-      <li>Address book</li>
-      <li>Transaction history view</li>
-      <li>Built-in Ethereum units converter</li>
-    </ul>
+    <section className={styles.featurePaneLeft}>
+      <section className={styles.info}>
+        <h2 className={styles.infoTitle}>All networks supported</h2>
+        <ul style={styles.infoList}>
+          <li>Connect to main network, all public test networks, as well as custom private networks.</li>
+          <li>Advanced network detection based on blockchain state.</li>
+        </ul>
+      </section>
+      <section className={styles.screenshot}>
+        <Img sizes={data.xpNetworks.childImageSharp.sizes} />
+      </section>
+    </section>
+    <section className={styles.featurePaneRight}>
+      <section className={styles.info}>
+        <h2 className={styles.infoTitle}>Full smart contract support</h2>
+        <ul style={styles.infoList}>
+          <li>Deploy smart contracts to any chain and easily save the contract address for later use.</li>
+          <li>Use the friendly UI to interact with any contract on any chain.</li>
+        </ul>
+      </section>
+      <section className={styles.screenshot}>
+        <Img sizes={data.xpContracts.childImageSharp.sizes} />
+      </section>
+    </section>
+    <section className={styles.featurePaneLeft}>
+      <section className={styles.info}>
+        <h2 className={styles.infoTitle}>Multi-tab browser with bookmarks</h2>
+        <ul style={styles.infoList}>
+          <li>Run multiple Dapps simultaneously with tabbed browing.</li>
+          <li>Save your favourite Dapps as bookmarks.</li>
+        </ul>
+      </section>
+      <section className={styles.screenshot}>
+        <Img sizes={data.xpBrowser.childImageSharp.sizes} />
+      </section>
+    </section>
+    <section className={styles.featurePaneRight}>
+      <section className={styles.info}>
+        <h2 className={styles.infoTitle}>Address book </h2>
+        <ul style={styles.infoList}>
+          <li>Add contract and account addresses to your own personal address book.</li>
+          <li>Save time later on by looking up addresses instead of typing them out.</li>
+        </ul>
+      </section>
+      <section className={styles.screenshot}>
+        <Img sizes={data.xpAddressBook.childImageSharp.sizes} />
+      </section>
+    </section>
+    <section className={styles.featurePaneLeft}>
+      <section className={styles.info}>
+        <h2 className={styles.infoTitle}>Transaction history</h2>
+        <ul style={styles.infoList}>
+          <li>View your per-network transaction history for all transactions created in Meth.</li>
+          <li>Instantly access detailed transaction information on <a href="http://etherscan.io/">Etherscan</a>.</li>
+        </ul>
+      </section>
+      <section className={styles.screenshot}>
+        <Img sizes={data.xpTransactions.childImageSharp.sizes} />
+      </section>
+    </section>
+    <section className={styles.featurePaneRight}>
+      <section className={styles.info}>
+        <h2 className={styles.infoTitle}>Secure cross-device sync</h2>
+        <ul style={styles.infoList}>
+          <li>All data is sync-ed in real-time to your other devices.</li>
+          <li>Strong client-side encryption (AES-256-GCM) applied prior to sync - no one else can ever read your data.</li>
+        </ul>
+      </section>
+      <section className={styles.screenshot}>
+        <Img sizes={data.xpSync.childImageSharp.sizes} />
+      </section>
+    </section>
   </div>
 )
 
 export const query = graphql`
   query indexImages {
-    placeIt: file(relativePath: { eq: "images/placeit.png" }) {
+    xpApp: file(relativePath: { eq: "images/xp_app.png" }) {
       childImageSharp {
-        sizes(maxWidth: 640, maxHeight: 480) {
+        sizes(maxWidth: 640) {
           ...GatsbyImageSharpSizes
         }
       }
     }
 
-    generateWalletScr: file(relativePath: { eq: "images/generateWalletScr.png" }) {
+    xpWallet: file(relativePath: { eq: "images/xp_wallet.png" }) {
       childImageSharp {
-        sizes(maxWidth: 270) {
+        sizes(maxWidth: 640) {
+          ...GatsbyImageSharpSizes
+        }
+      }
+    }
+
+    xpNetworks: file(relativePath: { eq: "images/xp_networks.png" }) {
+      childImageSharp {
+        sizes(maxWidth: 640) {
+          ...GatsbyImageSharpSizes
+        }
+      }
+    }
+
+    xpContracts: file(relativePath: { eq: "images/xp_contracts.png" }) {
+      childImageSharp {
+        sizes(maxWidth: 640) {
+          ...GatsbyImageSharpSizes
+        }
+      }
+    }
+
+    xpBrowser: file(relativePath: { eq: "images/xp_browser.png" }) {
+      childImageSharp {
+        sizes(maxWidth: 640) {
+          ...GatsbyImageSharpSizes
+        }
+      }
+    }
+
+    xpSync: file(relativePath: { eq: "images/xp_sync.png" }) {
+      childImageSharp {
+        sizes(maxWidth: 640) {
+          ...GatsbyImageSharpSizes
+        }
+      }
+    }
+
+    xpAddressBook: file(relativePath: { eq: "images/xp_addressbook.png" }) {
+      childImageSharp {
+        sizes(maxWidth: 640) {
+          ...GatsbyImageSharpSizes
+        }
+      }
+    }
+
+    xpTransactions: file(relativePath: { eq: "images/xp_transactions.png" }) {
+      childImageSharp {
+        sizes(maxWidth: 640) {
           ...GatsbyImageSharpSizes
         }
       }
